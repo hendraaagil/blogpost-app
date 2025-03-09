fa552336e231d5e9ea8d8bc4554def8de22e7105
hendraaagil <hendraaagil@gmail.com>
2025-03-09 13:34:00 +0700

    style: update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,11 @@ import type { ThemeConfig } from 'antd'
 const theme: ThemeConfig = {
   token: {
     fontSize: 16,
-    colorPrimary: '#0F172A',
+  },
+  components: {
+    Form: {
+      itemMarginBottom: 16,
+    },
   },
 }
 
