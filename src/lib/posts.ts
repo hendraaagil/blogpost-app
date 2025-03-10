e83e99f15a42389c879ad138a2ca80839110be97
hendraaagil <hendraaagil@gmail.com>
2025-03-11 02:42:17 +0700

    feat: include pagination metadata

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,23 +1,50 @@
 import axios from '@/lib/axios'
 import { useQuery } from '@tanstack/react-query'
 
+type Meta = {
+  pagination: {
+    total: number
+    pages: number
+    page: number
+    limit: number
+  }
+}
+
 type Post = {
   id: number
+  key: number
   title: string
   body: string
   user_id: number
 }
 
-const getPosts = async () => {
+type PostsResponse = { data: Post[]; meta: Meta }
+
+const getPosts = async (): Promise<PostsResponse> => {
   try {
-    const { data } = await axios.get<Post[]>('/posts')
-    return data.map((post) => ({
-      ...post,
-      key: post.id,
-    }))
+    const { data, headers } = await axios.get<Post[]>('/posts')
+    const meta = {
+      pagination: {
+        total: Number(headers['x-pagination-total']),
+        pages: Number(headers['x-pagination-pages']),
+        page: Number(headers['x-pagination-page']),
+        limit: Number(headers['x-pagination-limit']),
+      },
+    }
+
+    return {
+      data: data.map((post) => ({
+        ...post,
+        key: post.id,
+      })),
+      meta,
+    }
   } catch (error) {
     console.error(error)
-    return []
+    return {
+      data: [],
+      meta: { pagination: { total: 0, pages: 0, page: 0, limit: 0 } },
+    }
   }
 }
 
